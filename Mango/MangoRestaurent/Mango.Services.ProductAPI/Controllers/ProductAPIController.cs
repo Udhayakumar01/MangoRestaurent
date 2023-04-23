7c5d2c67086c8fa57e2bd3da388feb45a4ffd547
Udhayakumar S <86413988+Udhayakumar01@users.noreply.github.com>
2023-04-23 16:28:28 +0530

    Adding ProductAPIController

---
+++ Mango/MangoRestaurent/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,55 @@
+﻿using Mango.Services.ProductAPI.Model;
+using Mango.Services.ProductAPI.Model.Dto;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/Products")]
+    public class ProductAPIController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+        protected ResponseDto _response;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            this._response = new ResponseDto();  
+        }
+
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProduct();
+                _response.Result = productDtos;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string> { ex.Message.ToString() };
+            }
+            return _response;
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDtos = await _productRepository.GetProductById(id);
+                _response.Result = productDtos;
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessage = new List<string> { ex.Message.ToString() };
+            }
+            return _response;
+
+        }
+    }
+}
