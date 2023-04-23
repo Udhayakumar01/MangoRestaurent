4e59f10430af254ec54f59cf23e77bf67dac1853
Udhayakumar S <86413988+Udhayakumar01@users.noreply.github.com>
2023-04-23 14:48:32 +0530

    Declaring Methods in IProductRepository Interface

--- Mango/MangoRestaurent/Mango.Services.ProductAPI/Repository/IProductRepository.cs
+++ Mango/MangoRestaurent/Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Mango.Services.ProductAPI.Repository
+﻿using Mango.Services.ProductAPI.Model;
+
+namespace Mango.Services.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProduct();
+        Task<ProductDto> GetProductById(int productId);
+
+        Task<ProductDto> CreateUpdateProduct(ProductDto product);
+        Task<bool> DeleteProduct(int productId);
     }
 }
